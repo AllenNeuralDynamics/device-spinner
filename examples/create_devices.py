2f402cebb962df473c71c1801ed32e5acf999720
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2024-06-10 17:08:02 -0700

    add another basic example

---
+++ examples/create_devices.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Instantiate objects from list of dicts."""
+
+from device_spinner.device_spinner import DeviceSpinner
+import pprint
+
+# Uncomment for some prolific log statements.
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+logger.handlers[-1].setFormatter(
+   logging.Formatter(fmt='%(asctime)s:%(levelname)s: %(message)s'))
+
+
+device_specs = \
+{
+    "MyDict":
+        {
+            "module": "builtins",
+            "class": "dict",
+            "kwds": {"key0": "MyVal"},
+        },
+    "MyVal":
+        {
+            "module": "builtins",
+            "class": "str",
+            "args": ["my_val"],
+        },
+
+}
+factory = DeviceSpinner()
+device_trees = factory.create_devices_from_specs(device_specs)
