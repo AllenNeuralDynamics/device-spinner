cb9416d6f3bfcfb61d74b241a12510ff55176112
Sonya Vasquez <sonya.vasquez@alleninstitute.org>
2026-06-09 23:39:25 -0700

    remove kruft import; add constructor field

--- src/device_spinner/models.py
+++ src/device_spinner/models.py
@@ -1,17 +1,17 @@
-from multiprocessing import Value
-
+from inspect import signature
 from functools import cached_property
 from pydantic import BaseModel, RootModel, Field, model_validator
-from typing import Optional, Any, Self
+from typing import Optional, Any, Self, Callable
 import importlib
 
 class DeviceSpec(BaseModel):
     module_name: Optional[str] = Field(alias="module", default=None)
     class_name: Optional[str] = Field(alias="class", default=None)
     factory: Optional[str] = None
+    constructor_name: Optional[str] = Field(alias="constructor", default=None)
 
     args: Optional[list[Any]] = None
-    kwds: Optional[dict[str, Any]] = None
+    kwargs: Optional[dict[str, Any]] = Field(alias="kwds", default=None)
 
     skip_args: Optional[list[str]] = None
     skip_kwds: Optional[list[str]] = None
@@ -29,6 +29,37 @@ class DeviceSpec(BaseModel):
         else:
             module_path = self.module_name
         return module_path
+
+    @cached_property
+    def factory_fn_name(self) -> str:
+        # Simple case: return factory or class field directly.
+        if self.module_name is not None:
+            if self.class_name is not None:
+                return self.class_name
+            if self.factory is not None:
+                return self.factory
+        else:  # Deduce it from combined paths.
+            if self.class_name is not None:
+                return self.class_name.rsplit(".", 1)[-1]
+            if self.factory is not None:
+                return self.factory.rsplit(".", 1)[-1]
+        raise ValueError("Cannot deduce factory function name.")
+
+    @cached_property
+    def factory_fn(self) -> Callable[..., ...]:
+        """return a callable to instantiate the class instance.
+        Callable may be: (1) a factory function, (2) the class constructor,
+        (3) a factory method (factory function that belongs to the class)
+        """
+        # We're trying to do one of these 3 things:
+        #   from module import factory function.
+        #   from module import class.
+        #   from module import class; from class get factory method.
+        module = importlib.import_module(self.module_path)
+        class_or_factory = getattr(module, self.factory_fn_name)
+        if self.constructor_name is not None:
+            class_or_factory = getattr(class_or_factory, self.constructor_name)
+        return class_or_factory
 
     @model_validator(mode='after')
     def validate_class_or_factory(self) -> Self:
@@ -68,4 +99,25 @@ class DeviceSpec(BaseModel):
 
 
 class DeviceTrees(RootModel[dict[str, DeviceSpec]]):
-    pass
+
+    @model_validator(mode='after')
+    def validate_dependency_existence(self) -> Self:
+        """Iterate through all entries and ensure that any arg/kwarg values
+        specified as strings that are class instances in the function signature
+        exist elsewhere."""
+        #for instance_name, spec in self.root.items():
+        #    print(f"factory function is {spec.factory_fn}")
+        #    try:
+        #        sig = signature(spec.factory_fn)
+        #        print(sig.parameters)
+        #    except ValueError:
+        #        print(f"Cannot print parameters for {spec.factory_fn}.")
+        #    if spec.args is not None:
+        #        for index, arg in enumerate(spec.args):
+        #            # Check signature for any class type.
+        #            pass
+        #    if spec.kwargs is not None:
+        #        for name, val in spec.kwargs:
+        #            # Check signature for any class type.
+        #            pass
+        return self
